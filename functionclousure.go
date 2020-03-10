f900db062815628b265aa6e30b3fead23b857c30
Akshay Vijapur <akshayvijapur@gmail.com>
2020-03-10 13:16:14 +0530

    Simple closure functions
    Signed-off-by: Akshaykumar Vijapur <akshaykumar.vijapur@intel.com>

---
+++ functionclousure.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+//Function closure refers to variable which are outside its body.
+
+func test() func() int {
+	var i = 0 // i is declared outside the function.
+	return func() int {
+		i = i + 1 //But i is  accessible inside the function.
+		return i
+	}
+}
+
+func main() {
+	testfunc := test()
+	fmt.Println(testfunc())
+	fmt.Println(testfunc())
+	fmt.Println(testfunc())
+
+	testfunc1 := test()
+	fmt.Println(testfunc1())
+	fmt.Println(testfunc1())
+}
