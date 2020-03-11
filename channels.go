b3a366c992465c7d9cfabbd24fbcd069b08ee47f
Akshay Vijapur <akshayvijapur@gmail.com>
2020-03-11 14:46:39 +0530

    Go Channels
    Signed-off-by: Akshaykumar Vijapur <akshaykumar.vijapur@intel.com>

---
+++ channels.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// Channels are used to send and recive data.
+// data := <-ch get the data from channel
+// ch  <- data Send the data to channel
+func main() {
+	// Create a channel
+	ch := make(chan string)
+	go getMeData(ch) //Create a routine and send the channel.
+	data := <-ch     // data := <-ch Get the data.
+	fmt.Println(data)
+
+}
+
+func getMeData(ch chan string) {
+	data := "Hello World"
+	ch <- data //send the data to ch. ch <- data.
+}
